c468d35fcb1ae19b59b61970700e483ca0455e9d
Рома <roma219@yandex.ru>
2018-02-03 16:23:16 +0300

    revert wallet tests..

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,4 +1,3 @@
-/* eslint-env jest */
 import { createLocalVue } from 'vue-test-utils';
 import Vuex from 'vuex';
 import wallet from '../src/modules/wallet.js';
@@ -20,33 +19,32 @@ beforeAll(done => {
   store.dispatch('initApis', () => {
     console.log('initApis callback');
     done();
-  });
+  })
 });
 
 describe('wallet module', () => {
-  const brainkey = 'glink omental webless pschent knopper brumous scarry' +
-    ' were wasting isopod raper barbas maco kirn tegua mitome';
+  const brainkey = 'glink omental webless pschent knopper brumous scarry were wasting isopod raper barbas maco kirn tegua mitome';
   const password = 'qwer1234';
-  // const btsAsset = '1.3.0';
-  const testAccount = '1.2.383374';
-  // const hobbitAccount = '1.2.512210';
-  // const transferAmount = 10;
-  const ownerPubkey = 'BTS5AmuQyyhyzNyR5N3L6MoJUKiqZFgw7xTRnQr5XP5sLKbptCABX';
+  const bts_asset = '1.3.0';
+  const test_account = '1.2.383374';
+  const hobbit_account = '1.2.512210';
+  const transfer_amount = 10;
+  const owner_pubkey = 'BTS5AmuQyyhyzNyR5N3L6MoJUKiqZFgw7xTRnQr5XP5sLKbptCABX';
 
   it('creates wallet', done => {
-    store.dispatch('createWallet', { brainkey, password }).then(() => {
+    store.dispatch('createWallet', {brainkey, password}).then(() => {
       expect(store.getters.getBrainkey).toBe(brainkey);
-      const ownerKey = store.getters.getKeys.owner;
-      const computedOwnerPubkey = ownerKey.toPublicKey().toPublicKeyString();
-      expect(computedOwnerPubkey).toBe(ownerPubkey);
+      const owner_key = store.getters.getKeys.owner;
+      const computed_owner_pubkey = owner_key.toPublicKey().toPublicKeyString();
+      expect(computed_owner_pubkey).toBe(owner_pubkey);
       done();
     });
   }, 20000);
 
   it('validates password', done => {
     expect(store.getters.isValidPassword(password)).toBe(true);
-    expect(store.getters.isValidPassword('wrong password')).toBe(false);
-    done();
+    expect(store.getters.isValidPassword("wrong password")).toBe(false);
+    done()
   });
 
   it('locks wallet', done => {
@@ -54,20 +52,20 @@ describe('wallet module', () => {
     store.dispatch('lockWallet').then(() => {
       expect(store.getters.isLocked).toBe(true);
       done();
-    });
+    })
   });
 
   it('unlocks wallet', done => {
     store.dispatch('unlockWallet', password).then(() => {
-      const ownerKey = store.getters.getKeys.owner;
-      const computedOwnerPubkey = ownerKey.toPublicKey().toPublicKeyString();
-      expect(computedOwnerPubkey.substr(3)).toBe(ownerPubkey.substr(3));
+      const owner_key = store.getters.getKeys.owner;
+      const computed_owner_pubkey = owner_key.toPublicKey().toPublicKeyString();
+      expect(computed_owner_pubkey.substr(3)).toBe(owner_pubkey.substr(3));
       done();
-    });
+    })
   });
 
   it('fetches wallet user', done => {
-    expect(store.state.wallet.user_id).toBe(testAccount);
+    expect(store.state.wallet.user_id).toBe(test_account);
     done();
   });
 });
