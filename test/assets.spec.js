0dcc6a8ae74bed34b5a55dc04fd6d7b64309944e
Рома <roma219@yandex.ru>
2018-02-05 21:26:02 +0300

    tests for getters and initial state

--- test/assets.spec.js
+++ test/assets.spec.js
@@ -20,6 +20,35 @@ describe('Assets module', () => {
     });
   });
 
+  it('has correct initial state', () => {
+    expect(store.state.assets.assets).toEqual({});
+    expect(store.state.assets.defaultAssetsIds).toEqual([]);
+    expect(store.state.assets.defaultAssetsNames).toEqual(['BTS', 'OPEN.EOS', 'USD',
+      'OPEN.OMG', 'CNY', 'OPEN.LTC', 'OPEN.EOS', 'TRFND', 'OPEN.BTC', 'ARISTO', 'ARCOIN']);
+    expect(store.state.assets.pending).toBeFalsy();
+  });
+
+  it('has correct getters', () => {
+    expect(store.getters['assets/getAssets']).toEqual({});
+    expect(store.getters['assets/getDefaultAssetsIds']).toEqual([]);
+    expect(store.getters['assets/getDefaultAssetsNames']).toEqual(['BTS', 'OPEN.EOS', 'USD',
+      'OPEN.OMG', 'CNY', 'OPEN.LTC', 'OPEN.EOS', 'TRFND', 'OPEN.BTC', 'ARISTO', 'ARCOIN']);
+
+    const testAssets = {
+      '1.3.0': {
+        symbol: 'BTS'
+      },
+      '1.3.113': {
+        symbol: 'BTS'
+      }
+    };
+
+    store.state.assets.assets = testAssets;
+    expect(store.getters['assets/getAssets']).toEqual(testAssets);
+    expect(store.getters['assets/getAssetById']('1.3.0')).toEqual(testAssets['1.3.0']);
+    expect(store.getters['assets/getAssetById']('aaaa')).toBeFalsy();
+  });
+
   it('fetches assets', done => {
     store.dispatch('assets/fetchAssets', ['1.3.0', '1.3.113']).then(() => {
       const recievedAssets = store.getters['assets/getAssets'];
