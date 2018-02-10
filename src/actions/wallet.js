854ebe8b5e41010021a4328969e8181bf4334292
michaelenco <michael.enco@gmail.com>
2018-02-10 07:15:05 +0300

    api mock and module refactor

--- src/actions/wallet.js
+++ src/actions/wallet.js
@@ -1,7 +1,10 @@
 import { PrivateKey, key, Aes } from 'bitsharesjs';
 import * as types from '../mutations';
+import { getAccountIdByOwnerPubkey, getAccount } from '../services/wallet.js';
 
-export const createWallet = ({ commit }, { brainkey, password }) => {
+const OWNER_KEY_INDEX = 1;
+
+export const createWallet = async ({ commit }, { brainkey, password }) => {
   const passwordAes = Aes.fromSeed(password);
   const encryptionBuffer = key.get_random_key().toBuffer();
   const encryptionKey = passwordAes.encryptToHex(encryptionBuffer);
@@ -12,23 +15,65 @@ export const createWallet = ({ commit }, { brainkey, password }) => {
   const passwordPrivate = PrivateKey.fromSeed(password);
   const passwordPubkey = passwordPrivate.toPublicKey().toPublicKeyString();
 
-  const keys = {
+  const ownerKey = key.get_brainPrivateKey(brainkey, OWNER_KEY_INDEX);
+  const ownerPubkey = ownerKey.toPublicKey().toPublicKeyString();
+
+  const result = {
     passwordPubkey,
     encryptionKey,
     encryptedBrainkey,
-    aesPrivate
+    aesPrivate,
+    userId: await getAccountIdByOwnerPubkey(ownerPubkey)
   };
 
-  commit(types.WALLET_CREATED, keys);
+  commit(types.WALLET_CREATED, result);
 };
 
 export const unlockWallet = ({ commit, state }, password) => {
   const passwordAes = Aes.fromSeed(password);
-  const encryptionPlainbuffer = passwordAes.decryptHexToBuffer(state.encryption_key);
+  const encryptionPlainbuffer = passwordAes.decryptHexToBuffer(state.encryptionKey);
   const aesPrivate = Aes.fromSeed(encryptionPlainbuffer);
   commit(types.WALLET_UNLOCK, aesPrivate);
 };
 
 export const lockWallet = ({ commit }) => {
   commit(types.WALLET_LOCK);
+};
+
+export const createAccount = async ({ commit, state, getters }, {
+  name,
+  referrer,
+  faucetUrl = 'https://faucet.bitshares.eu/onboarding'
+}) => {
+  const { active, owner } = getters['getKeys'];
+  try {
+    const response = await fetch(faucetUrl + '/api/v1/accounts', {
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        account: {
+          name,
+          owner_key: owner.toPublicKey().toPublicKeyString(),
+          active_key: active.toPublicKey().toPublicKeyString(),
+          memo_key: active.toPublicKey().toPublicKeyString(),
+          refcode: null,
+          referrer
+        }
+      })
+    })
+    const result = response.json();
+    if (result.error) {
+      const { error: { base: [errorText] } } = result.error;
+      throw Error(errorText);
+    } else {
+      const { id } = await getAccount(name);
+      commit(types.WALLET_ACCOUNT_CREATED, id);
+    }
+  } catch (error) {
+    commit(types.WALLET_ACCOUNT_CREATE_ERROR, error);
+  }
 };
---
+++ test/wallet.spec.js
@@ -0,0 +1,99 @@
+/* eslint-env jest */
+
+import { createLocalVue } from 'vue-test-utils';
+import Vuex from 'vuex';
+import wallet from '../src/modules/wallet.js';
+import { getAccount, suggestBrainkey } from '../src/services/wallet.js';
+import dictionary from './brainkey_dictionary.js';
+
+jest.mock('bitsharesjs-ws');
+
+// eslint-disable-next-line max-len
+const brainkey = 'glink omental webless pschent knopper brumous scarry were wasting isopod raper barbas maco kirn tegua mitome';
+const password = 'qwer1234';
+const testAccount = '1.2.383374';
+const testAccountName = 'anlopan364test2';
+const hobbitAccount = '1.2.512210';
+const hobbitAccountName = 'hobb1t';
+const ownerPubkey = 'BTS5AmuQyyhyzNyR5N3L6MoJUKiqZFgw7xTRnQr5XP5sLKbptCABX';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {
+    wallet
+  }
+});
+
+describe('wallet module', () => {
+  it('creates wallet', async done => {
+    await store.dispatch('wallet/createWallet', { brainkey, password });
+    expect(store.getters['wallet/getBrainkey']).toBe(brainkey);
+    const ownerKey = store.getters['wallet/getKeys'].owner;
+    const computedOwnerPubkey = ownerKey.toPublicKey().toPublicKeyString();
+    expect(computedOwnerPubkey).toBe(ownerPubkey);
+    done();
+  });
+
+  it('validates password', done => {
+    expect(store.getters['wallet/isValidPassword'](password)).toBe(true);
+    expect(store.getters['wallet/isValidPassword']('wrong password')).toBe(false);
+    done();
+  });
+
+  it('unlocks wallet', async done => {
+    await store.dispatch('wallet/unlockWallet', password);
+    expect(store.getters['wallet/getBrainkey']).toBe(brainkey);
+    expect(store.getters['wallet/isLocked']).toBe(false);
+    done();
+  });
+
+  it('locks wallet', async done => {
+    await store.dispatch('wallet/lockWallet');
+    expect(store.getters['wallet/isLocked']).toBe(true);
+    done();
+  });
+
+  it('checks existing accounts', async done => {
+    const acc = await getAccount(hobbitAccountName)
+    expect(acc).not.toBe(null);
+    const nonExistingAcc = await getAccount('definitely not existing account')
+    expect(nonExistingAcc).toBe(null);
+    done();
+  });
+
+  it('creates account', async done => {
+    const generatedBrainkey = suggestBrainkey(dictionary.en);
+    await store.dispatch('wallet/createWallet', { brainkey: generatedBrainkey, password });
+    const { owner, active } = store.getters['wallet/getKeys'];
+    const walletOwnerPubkey = owner.toPublicKey().toPublicKeyString();
+    const activePubkey = active.toPublicKey().toPublicKeyString();
+    const name = hobbitAccountName;
+
+    // simulate success response
+    global.fetch = () => {
+      return new Promise(resolve => {
+        const res = {
+          account: {
+            active_key: activePubkey,
+            memo_key: activePubkey,
+            name,
+            owner_key: walletOwnerPubkey,
+            referrer: 'referrer',
+            registrar: 'registrar'
+          }
+        };
+        const response = {
+          json: () => res
+        };
+        resolve(response);
+      });
+    };
+
+
+    await store.dispatch('wallet/createAccount', { name });
+    expect(store.state.wallet.userId).toBe(hobbitAccount);
+    done();
+  });
+});
