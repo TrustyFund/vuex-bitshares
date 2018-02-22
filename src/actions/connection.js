27b4c531d00eb5eaf5ff5de1dc86258be7dcd03f
Рома <roma219@yandex.ru>
2018-02-22 10:52:39 +0800

    reconnect and closed handling

--- src/actions/connection.js
+++ src/actions/connection.js
@@ -6,15 +6,15 @@ import * as types from '../mutations';
  */
 export const initConnection = ({ commit, getters }) => {
   const updateConnectionStatus = (status) => {
-    const prevStatus = getters.getRpcStatus;
+    // const prevStatus = getters.getRpcStatus;
     const wsConnected = getters.isWsConnected;
     console.log('connection status : ', status);
-    if (status === 'reconnect' || status === 'close') {
-      API.connect(updateConnectionStatus);
-    }
+    // if (status === 'reconnect') API.connect(updateConnectionStatus);
     commit(types.RPC_STATUS_UPDATE, { status });
-    if (prevStatus === null && status === 'error') commit(types.WS_DISCONNECTED);
-    if (!wsConnected && status === 'realopen') commit(types.WS_CONNECTED);
+    if (status === 'error' || status === 'closed') commit(types.WS_DISCONNECTED);
+    if (!wsConnected && (status === 'realopen' || status === 'reconnect')) {
+      commit(types.WS_CONNECTED);
+    }
   };
 
   API.connect(updateConnectionStatus);
