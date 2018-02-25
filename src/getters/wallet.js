f841641c6f622e68a052984461ba9426310cebfc
michaelenco <michael.enco@gmail.com>
2018-02-25 14:40:01 +0300

    fix: linted

--- src/getters/wallet.js
+++ src/getters/wallet.js
@@ -1,5 +1,4 @@
-import { PrivateKey, key, Aes, TransactionBuilder, TransactionHelper } from 'bitsharesjs';
-import { getAccount } from '../services/wallet';
+import { PrivateKey, key, Aes, TransactionHelper } from 'bitsharesjs';
 
 const ACTIVE_KEY_INDEX = 0;
 const OWNER_KEY_INDEX = 1;
@@ -34,7 +33,6 @@ export const isLocked = state => {
 
 export const encryptMemo = state => {
   return (memo, toPubkey) => {
-
     const { active } = getKeys(state);
     const activePubkey = active.toPublicKey().toPublicKeyString();
     const nonce = TransactionHelper.unique_nonce_uint64();
@@ -53,20 +51,20 @@ export const encryptMemo = state => {
       message
     };
   };
-}
+};
 
 export const signTransaction = state => {
   return async transaction => {
     const { active, owner } = getKeys(state);
-    const pubkeys = [active, owner].map(key => key.toPublicKey().toPublicKeyString());
+    const pubkeys = [active, owner].map(privkey => privkey.toPublicKey().toPublicKeyString());
     const requiredPubkeys = await transaction.get_required_signatures(pubkeys);
-    for (let requiredPubkey of requiredPubkeys) {
-      if (active.toPublicKey().toPublicKeyString() == requiredPubkey) {
+    requiredPubkeys.forEach(requiredPubkey => {
+      if (active.toPublicKey().toPublicKeyString() === requiredPubkey) {
         transaction.add_signer(active, requiredPubkey);
       }
-      if (owner.toPublicKey().toPublicKeyString() == requiredPubkey) {
+      if (owner.toPublicKey().toPublicKeyString() === requiredPubkey) {
         transaction.add_signer(owner, requiredPubkey);
       }
-    }
+    });
   };
-}
+};
