79fef5c16d687ef2d56289461f6d94ee94996c77
ololokiras <bellendirr@yandex.ru>
2018-04-10 19:15:28 +0800

    repair asset cleanup

--- test/api.spec.js
+++ test/api.spec.js
@@ -7,7 +7,6 @@ jest.mock('../src/services/api/assets.js');
 describe('Mocked Assets API', () => {
   it('api mock works', async () => {
     const result = await API.Assets.fetch(['1.3.121', '1.3.0']);
-    console.log(result);
     expect(result[0].symbol).toBe('USD');
     expect(result[1].symbol).toBe('BTS');
   });
