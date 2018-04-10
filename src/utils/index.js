165a7f01c1021145e6c88c48a40db0be8d773b68
Рома <roma219@yandex.ru>
2018-04-10 18:25:05 +0800

    clean utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,7 +15,7 @@ export const arrayToObject = (array) => {
  * Returns array containing first and last history prices of asset.
  * @param {Array} history - array with asset's history data
  */
-export const getPrices = (history, id, days) => {
+export const getPrices = (history, id) => {
   if (!history.length) return { first: 0, last: 0 };
   const startElem = history[0];
   const endElem = history[history.length - 1];
@@ -53,20 +53,6 @@ export const formatPrices = (prices, base, quote) => {
   prices.first = Math.abs(prices.first);
   prices.last = Math.abs(prices.last);
   return prices;
-};
-
-/**
- * Returns amount of change by percent, calculated by prices history and exchange multiplier
- * @param {Object} object.prices - object with asset's history prices
- * @param {number} object.prices.first - first price of asset history
- * @param {number} object.prices.last - last price of asset history (current)
- * @param {Object} object.multiplier - object with base -> fiat exchange rates
- * @param {number} object.multiplier.first - multiplier for first history price
- * @param {number} object.multiplier.last - multiplier for last history price (current)
- */
-export const calcPercentChange = (prices, multiplier) => {
-  return ((((prices.last * multiplier.last) /
-    (prices.first * multiplier.first)) * 100) - 100);
 };
 
 export const encryptMemo = (memo, fromKey, toPubkey) => {
@@ -225,41 +211,4 @@ export const getValuesToUpdate = (balances, baseBalances, update) => {
     }
   });
   return result;
-};
-
-export const calcPortfolioItem = ({
-  asset,
-  history24,
-  history7,
-  baseAsset,
-  fiatMultiplier,
-  marketPrice,
-  balance }) => {
-  const multiplier = fiatMultiplier;
-
-  const price = marketPrice || history24.last || history7.last;
-
-  const baseValue = parseInt((balance * price).toFixed(0), 10);
-
-  const baseValuePrecised = baseValue / (10 ** baseAsset.precision);
-
-  const fiatValue = parseInt((baseValue * fiatMultiplier.last).toFixed(0), 10);
-
-  let change24 = calcPercentChange(history24, multiplier);
-  if (history24.fist === history24.last && asset.id !== baseAsset.id) change24 = 0;
-
-  let change7 = calcPercentChange(history7, multiplier);
-  if (history7.fist === history7.last && asset.id !== baseAsset.id) change7 = 0;
-
-  return {
-    name: asset.symbol,
-    balance,
-    baseValue,
-    baseValuePrecised,
-    basePrecision: baseAsset.precision,
-    fiatValue,
-    change24,
-    change7,
-    price
-  };
 };
