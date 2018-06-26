c43595c79211249d83ff01860d8bd5297948a317
Anton Lopan <anlopan@gmail.com>
2018-06-26 18:46:37 +0300

    Add nowcoi

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ const config = {
     defaultNode: 'wss://bitshares.openledger.info/ws'
   },
   defaultAssetsNames: ['USD', 'OPEN.BTC', 'OPEN.ETH', 'OPEN.DASH', 'OPEN.LTC',
-    'OPEN.EOS', 'OPEN.STEEM', 'BTS', 'TRUSTY', 'TWENTIX'],
+    'OPEN.EOS', 'OPEN.STEEM', 'BTS', 'TRUSTY', 'TWENTIX', 'NOWCOIN'],
   referrer: 'trfnd',
   removePrefix: 'OPEN.',
   faucetUrl: 'https://faucet.trusty.fund/signup',
