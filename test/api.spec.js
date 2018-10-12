0e37ae2f8f624a2e3d12a827a3f34086636476b5
Рома <roma219@yandex.ru>
2018-10-12 21:30:56 +0300

    fix tests

--- test/api.spec.js
+++ test/api.spec.js
@@ -2,13 +2,12 @@
 // Testing mocked APIs here
 import API from '../src/services/api.js';
 
-// jest.mock('../src/services/api/assets.js');
+jest.mock('../src/services/api/assets.js');
 
 describe('Mocked Assets API', () => {
   it('api mock works', async () => {
-    // const result = await API.Assets.fetch(['1.3.121', '1.3.0']);
-    // expect(result[0].symbol).toBe('USD');
-    // expect(result[1].symbol).toBe('BTS');
-    expect(true).toBe(true)
+    const result = await API.Assets.fetch(['1.3.121', '1.3.0']);
+    expect(result[0].symbol).toBe('USD');
+    expect(result[1].symbol).toBe('BTS');
   });
 });
